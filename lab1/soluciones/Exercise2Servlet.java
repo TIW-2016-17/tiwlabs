ae433fb3afd44e111c0d0c3c80feafddefa15ff5
Carlos Tessier <prof.carlos.tessier@gmail.com>
2016-10-14 07:47:54 +0200

    Update and rename Ejercicio2Servlet.java to Exercise2Servlet.java

--- lab1/soluciones/Exercise2Servlet.java
+++ lab1/soluciones/Exercise2Servlet.java
@@ -11,63 +11,65 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 
- * @author David Palomar
- *
+ * Servlet implementation class Exercise2Servlet
  */
-@WebServlet("/cabeceras")
-public class Ejercicio2Servlet extends HttpServlet {
-	/**
-	 * 
-	 */
+@WebServlet("/headers")
+public class Exercise2Servlet extends HttpServlet {
+
 	private static final String NEW_LINE = "\n";
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Ejercicio2Servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	public Exercise2Servlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		StringBuilder sb = new StringBuilder();
-		
+
 		String protocol = request.getProtocol();
 		String url = request.getRequestURL().toString();
 		String method = request.getMethod();
 		String language = request.getLocale().getDisplayLanguage();
 		String remoteAddress = request.getRemoteAddr();
 
-		sb.append("PROTOCOL="+protocol);
+		sb.append("PROTOCOL=");
+		sb.append(protocol);
 		sb.append(NEW_LINE);
-		sb.append("URL="+url);
+		sb.append("URL=");
+		sb.append(url);
 		sb.append(NEW_LINE);
-		sb.append("METHOD="+method);
+		sb.append("METHOD=");
+		sb.append(method);
 		sb.append(NEW_LINE);
-		sb.append("LANGUAGE="+language);
+		sb.append("LANGUAGE=");
+		sb.append(language);
 		sb.append(NEW_LINE);
-		sb.append("REMOTE ADDRESS="+remoteAddress);
+		sb.append("REMOTE ADDRESS=");
+		sb.append(remoteAddress);
 		sb.append(NEW_LINE);
-		
+
 		Enumeration<String> headerNames = request.getHeaderNames();
 		while (headerNames.hasMoreElements()) {
 			String headerName = (String) headerNames.nextElement();
 			String headerValue = request.getHeader(headerName);
-			sb.append(headerName+"="+headerValue);
+			sb.append(headerName + "=" + headerValue);
 			sb.append(NEW_LINE);
-			
+
 		}
 		PrintWriter out = response.getWriter();
 		out.println(sb.toString());
 		out.close();
-		
-		
+
 	}
 
 }
