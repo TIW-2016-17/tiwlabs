f324c9a612838e54486b86d12905d560248204e4
Carlos Tessier <prof.carlos.tessier@gmail.com>
2016-11-11 08:29:35 +0100

    Update MyFirstController.java

--- lab7/solutions/exercise-4/MyFirstController.java
+++ lab7/solutions/exercise-4/MyFirstController.java
@@ -1,6 +1,7 @@
 package es.uc3m.tiw.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyFirstController {
 
-	@RequestMapping("/greet")
-	public @ResponseBody String greet(){
+	@RequestMapping("/greet/{name}")
+	public @ResponseBody String greet(Model model, @PathVariable String name){
+		model.addAttribute("name", name);
 		return "Hi";
 	}
   
